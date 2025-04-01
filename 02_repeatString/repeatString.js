60f9aacc8041b60d50c8ed5b0defdb4c3acc3627
CyberDagger <cyberdagger003@gmail.com>
2025-04-01 11:44:00 +0000

    Completed exercise 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function (string, times) {
+  let final = "";
+  if (times < 0) {
+    return "ERROR";
+  }
+  for (i = 0; i < times; i++) {
+    final += string;
+  }
+  return final;
 };
 
 // Do not edit below this line
