05376f3c75b642ef87d418c36aa4f1a1320eae5b
CyberDagger <cyberdagger003@gmail.com>
2025-04-01 14:21:00 +0000

    Exercise 5 completed

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,27 @@
-const sumAll = function() {
+const sumAll = function(firstNumber, secondNumber) {
+  let smallNumber;
+  let largeNumber;
+  let sum = 0;
 
+  if (!(Number.isInteger(firstNumber) && Number.isInteger(secondNumber))) {
+    return "ERROR";
+  } else if (firstNumber < 0 || secondNumber < 0) {
+    return "ERROR";
+  }
+    
+  if (firstNumber > secondNumber) {
+    smallNumber = secondNumber;
+    largeNumber = firstNumber;
+  } else {
+    smallNumber = firstNumber;
+    largeNumber = secondNumber;
+  }
+  
+  while (smallNumber <= largeNumber) {
+    sum += smallNumber;
+    smallNumber++;
+  }
+  return sum;
 };
 
 // Do not edit below this line
