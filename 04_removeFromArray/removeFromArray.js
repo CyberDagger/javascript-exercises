112579d731c92ec68554aad892495b02cc92916e
CyberDagger <cyberdagger003@gmail.com>
2025-04-01 12:38:13 +0000

    Completed exercise 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,11 @@
-const removeFromArray = function() {
+const removeFromArray = function(inArray, ...toRemove) {
+  for (let i = 0; i < inArray.length ; i++) {
+    if (toRemove.includes(inArray[i])) {
+      inArray.splice(i, 1);
+      i--;
+    }
+  }
+  return inArray;
 };
 
 // Do not edit below this line
