5c129cd7932e85667e04b812ca1563eacdebca9d
CyberDagger <cyberdagger003@gmail.com>
2025-04-01 12:02:32 +0000

    Completed exercise 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(originalString) {
+  let reversedString = "";
+  for (let i = originalString.length; i > 0; i--) {
+    reversedString += originalString.slice(i - 1, i);
+  }
+  return reversedString;
 };
 
 // Do not edit below this line
