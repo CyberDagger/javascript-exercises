83c46b30e5b634f14ca4950ae377d93922974db6
FValente003 <cyberdagger003@gmail.com>
2025-04-20 00:31:31 +0100

    Exercise 8 completed

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,38 @@
-const add = function() {
-	
+const add = function(x, y) {
+	return x + y;
 };
 
-const subtract = function() {
-	
+const subtract = function(x, y) {
+	return x - y;
 };
 
-const sum = function() {
-	
+const sum = function(args) {
+	let result = 0;
+  args.forEach(x => result += x);
+  return result;
 };
 
-const multiply = function() {
-
+const multiply = function(args) {
+  let result = 1;
+  args.forEach(x => result *= x);
+  return result;
 };
 
-const power = function() {
-	
+const power = function(a, x) {
+  let result = a;
+	for(let i = 1; i < x; i++) {
+    result *= a;
+  }
+  return result;
 };
 
-const factorial = function() {
-	
+const factorial = function(x) {
+	let result = 1;
+  while(x >= 1) {
+    result *= x;
+    x--;
+  }
+  return result;
 };
 
 // Do not edit below this line
