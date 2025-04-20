6b5ed2a8027ea584b0a76ec1c4452326df02f0b6
FValente003 <cyberdagger003@gmail.com>
2025-04-20 17:18:15 +0100

    Exercise 10 completed

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,18 @@
-const fibonacci = function() {
-
+const fibonacci = function(x) {
+    let a = 0;
+    let b = 1;
+    x = parseInt(x);
+    if (x < 0) {
+        return "OOPS";
+    }
+    for (let i = 0; i < x; i++) {
+        a += b;
+        // a will have the higher value after the addition, so the following operations swap their values to retain the order
+        a = a + b;
+        b = a - b;
+        a = a - b;
+    }
+    return a;
 };
 
 // Do not edit below this line
