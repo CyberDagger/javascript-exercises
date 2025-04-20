152faf7e40b4f5476d4a5ce21b344f7e6f8389ea
FValente003 <cyberdagger003@gmail.com>
2025-04-20 19:54:44 +0100

    Exercise 12 completed

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,21 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    const d = new Date();
+    currentYear = d.getFullYear();
+    let oldest;
+    if (!("yearOfDeath" in people[0])) {
+        people[0].yearOfDeath = currentYear;
+    }
+    oldest = people.reduce((current, person) => {
+        if (!("yearOfDeath" in person)) {
+            person.yearOfDeath = currentYear;
+        }
+        if (current.yearOfDeath - current.yearOfBirth < person.yearOfDeath - person.yearOfBirth) {
+            return person;
+        } else {
+            return current;
+        }
+    })
+    return oldest;
 };
 
 // Do not edit below this line
