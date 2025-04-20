a344a5ecef91fa468569afd8f0028d35acb369e9
FValente003 <cyberdagger003@gmail.com>
2025-04-20 16:54:43 +0100

    Exercise 9 completed

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,23 @@
-const palindromes = function () {
-
+const palindromes = function (word) {
+    const skipChars = new RegExp('[,.!? ]');
+    let i = 0;
+    let j = word.length - 1;
+    while(i < j) {
+        if (word.charAt(i).toLowerCase() != word.charAt(j).toLowerCase()) {
+            if (skipChars.test(word.charAt(i))) {
+                i++;
+            }
+            else if (skipChars.test(word.charAt(j))) {
+                j--;
+            } else {
+                return false;
+            }
+        } else {
+            i++;
+            j--;
+        }
+    }
+    return true;
 };
 
 // Do not edit below this line
